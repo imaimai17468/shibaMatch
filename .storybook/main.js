4c49cbfff066048091f87dab1d4bebfcb3add1ff
imaimai17468 <tosiki_801_life@outlook.jp>
2023-01-15 21:28:59 +0900

    [feat]環境構築

---
+++ .storybook/main.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const rootPath = path.resolve(__dirname, '../src/')
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-scss',
+    'storybook-addon-next-router',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias['@'] = rootPath
+    return config
+  },
+  staticDirs: ['../public'],
+}
