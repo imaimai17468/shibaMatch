83b585caf5c083d75db10e86f63d8f7ea36a39e9
imaimai17468 <tosiki_801_life@outlook.jp>
2023-01-20 01:10:04 +0900

    [feat]画像をrecoilで保存するように

--- src/components/common/Picture/Picture.tsx
+++ src/components/common/Picture/Picture.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
+import { useRecoilState } from 'recoil'
+import { favoriteState } from '@/src/store/favorite'
 import Image from 'next/image'
 import { Favorite } from '../Favorite'
 import { PictureProps } from './Picture.types'
@@ -11,17 +13,34 @@ const Picture: React.FC<PictureProps> = ({
   width,
   height,
 }) => {
+  const [favorite, setFavorite] = useRecoilState(favoriteState)
+  const isFavorite = useMemo(() => favorite.includes(src), [favorite, src])
+
+  const favoriteHandler = (src: string) => {
+    const isFavorite = favorite.includes(src)
+    if (isFavorite) {
+      setFavorite(favorite.filter((item) => item !== src))
+    } else {
+      setFavorite([...favorite, src])
+    }
+  }
+
   return (
     <div className="relative">
       <Image
         key={index}
         src={src}
         alt={'shiba' + index}
-        width={width || 200}
-        height={height || 200}
+        width={width || 150}
+        height={height || 150}
         className={className + ' rounded-full shadow-lg'}
       />
-      <Favorite className="absolute top-0 right-0" />
+      <Favorite
+        className="absolute top-0 right-0"
+        onClick={favoriteHandler}
+        src={src}
+        iniFavorite={isFavorite}
+      />
     </div>
   )
 }
