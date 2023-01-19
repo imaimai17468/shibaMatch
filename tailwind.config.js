805c4545c09b8272f52f226cd1cae515d32b3ab7
imaimai17468 <tosiki_801_life@outlook.jp>
2023-01-19 19:44:59 +0900

    [fix]tailwindの設定を新しい書き方に変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,9 @@
 module.exports = {
   mode: 'jit',
-  darkMode: false, // 'media' or 'class'
-  purge: {
-    content: [
-      './src/pages/**/*.{js,ts,jsx,tsx}',
-      './src/components/**/*.{js,ts,jsx,tsx}'
-    ],
-    options: {
-      // https://purgecss.com/safelisting.html#patterns
-      safelist: {
-        standard: [/^bg-/, /^text-/],
-      },
-    },
-  },
-  content: [],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
