59ef10cbdc8cee33cfbdb8cf1359598e778d68a1
imaimai17468 <tosiki_801_life@outlook.jp>
2023-01-19 19:44:14 +0900

    [feat]画像のurlの設定を追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        // https://cdn.shibe.online/shibes/dba6f4e199d09b95d0fea7d2497b319afe3c0ef5.jpg
+        protocol: 'https',
+        hostname: 'cdn.shibe.online',
+        port: '',
+        pathname: '/shibes/*',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
